25368d930b4dd25fcdd021ebaf548a1f96afa457
fathi <fathi.murad001@stud.fh-dortmund.de>
2022-08-04 20:10:41 +0200

    Klausur_07_02_2022

--- src/alte_klausuren_programmieraufgaben/Klausur_07_02_2022.java
+++ src/alte_klausuren_programmieraufgaben/Klausur_07_02_2022.java
@@ -20,12 +20,16 @@ public class Klausur_07_02_2022 {
 	
 	// Aufgabe 4, b)
 	boolean istBinär(BKnoten knoten) {
-		if (knoten == null) return true; // leerer Baum ist binär
-		if (knoten.kinder.length > 2){ // Wenn ein Knoten mehr als 2 Kinder besitzt, kann der Baum nicht mehr binär sein
+		if (knoten == null)
+			return true; // leerer Baum ist binär
+		if (knoten.kinder.length > 2) // Wenn ein Knoten mehr als 2 Kinder besitzt, kann der Baum nicht mehr binär sein
 			return false;
-		}
-		if(knoten.kinder[0] != null) istBinär(knoten.kinder[0]); // Gibt es ein linkes Kind? Wenn ja besuche es rekusiv
-		if(knoten.kinder[1] != null) istBinär(knoten.kinder[1]); // Gibt es ein rechtes Kind? Wenn ja besuche es rekusiv
+		
+		if(knoten.kinder[0] != null) // Gibt es ein linkes Kind? Wenn ja besuche es rekusiv
+			istBinär(knoten.kinder[0]);
+		if(knoten.kinder[1] != null) // Gibt es ein rechtes Kind? Wenn ja besuche es rekusiv
+			istBinär(knoten.kinder[1]);
+		
 		return true; // Wenn der allerletzte Knoten diese Zeile erreicht hat, dann haben alle Knoten bis zu 2 Kinder. Wenn sonst ein Knoten die "if (knoten.kinder.length > 2)" erfüllt, gibt die Methode direkt false zurück und bricht ab.
 	}
 }
